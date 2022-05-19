e7db9c42cfd27d0cd4b4b6ac97bd5793e8ddf892
I AM BOB <iambob314@gmail.com>
2022-05-19 00:25:09 -0400

    Replaced strings tables with default Tribes ones

--- mods/std/strings/bmps.strings.cs
+++
@@ -1,8 +0,0 @@
-// Custom BMP tags
-//
-// Start with 00161000
-//
-
-IDBMP_GUI_MAINMENU_EMBLEM = 00161000, "resources\\GUI\\Emblem.bmp";
-IDBMP_GUI_TRANSPARENT 	  = 00161001, "resources\\GUI\\transparent.bmp";
-IDBMP_GUI_BLACK 	  = 00161002, "Black.bmp";
