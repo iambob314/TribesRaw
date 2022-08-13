514a0ff31b530510738dd485dd14b83300253300
I AM BOB <iambob314@gmail.com>
2022-08-13 00:57:54 -0400

    Remove junk file and add more .gitignore rules

--- Temp/clipbuffer.cs
+++
@@ -1,9 +0,0 @@
-//--- export object begin ---//
-instant InteriorShape {
-	fileName = "bridgesection1.dis";
-	isContainer = "1";
-	position = "0 0 0";
-	rotation = "0 0 0";
-	lightParams = "0 ";
-};
-//--- export object end ---//
